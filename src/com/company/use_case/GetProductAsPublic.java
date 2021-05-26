708342340d2386885d973f6a7c8915692872ddfe
DjadjiT <djadji.traore75@gmail.com>
2021-05-26 20:07:32 +0200

    upd

--- src/com/company/use_case/GetProductAsPublic.java
+++ src/com/company/use_case/GetProductAsPublic.java
@@ -7,6 +7,7 @@ import com.company.model.*;
 import java.util.Optional;
 
 public class GetProductAsPublic {
+
     private PriceRepository priceRepository;
     private ProductRepository productRepository;
 
