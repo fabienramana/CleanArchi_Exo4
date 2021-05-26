5220a9f41aa7cac5f76a7223e9736c2a243abe68
DjadjiT <djadji.traore75@gmail.com>
2021-05-26 18:42:37 +0200

    update the interface

--- src/com/company/model/SellHistoryRepository.java
+++ src/com/company/model/SellHistoryRepository.java
@@ -1,5 +1,9 @@
 package com.company.model;
 
+import java.util.List;
+
 public interface SellHistoryRepository {
-    public SellHistory findById(int id);
+    public SellHistory findByProductIdAndUserId(int id);
+
+    public List<SellHistory> findByProductIdAndUserId(int productId, int userId);
 }
