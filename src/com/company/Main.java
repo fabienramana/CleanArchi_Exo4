561e03eddc41fb3d6fb6ecadc495aedabc786593
fabienramana <fabien.rmnd@gmail.com>
2021-05-26 20:22:46 +0200

    remove initData

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,15 +15,13 @@ public class Main {
 
         ProductDto p = getProduct.getProduct(1, java.util.Optional.empty());
         System.out.println(p.toString());
-
+        
+        //SCENARIO 1
         ProductDto p1 = getProduct.getProduct(1, java.util.Optional.of(1));
         System.out.println(p1.toString());
 
+        // SCENARIO 2
         ProductDto p2 = getProduct.getProduct(2, java.util.Optional.of(1));
         System.out.println(p2.toString());
-    }
-    
-    public void initData(){
-        
     }
 }
